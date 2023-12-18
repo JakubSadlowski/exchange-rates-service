39eeace4bc318ebe055855138df1e2f41a7a1c03
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2023-12-18 21:12:20 +0100

    #1 Initial spring boot setup

---
+++ src/test/java/com/js/exchangeratesservice/ExchangeRatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.exchangeratesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
