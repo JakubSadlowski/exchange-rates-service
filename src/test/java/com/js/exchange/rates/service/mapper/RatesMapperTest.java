31fc59e515de4feb4f24ef38e07f5987fded753e
M.Sadlowski <marek.sadlowski@gmail.com>
2024-01-04 22:30:42 +0100

    Fixed dependency conflict between spring-boot and mybatis

--- src/test/java/com/js/exchange/rates/service/mapper/RatesMapperTest.java
+++ src/test/java/com/js/exchange/rates/service/mapper/RatesMapperTest.java
@@ -3,17 +3,15 @@ package com.js.exchange.rates.service.mapper;
 import com.js.exchange.rates.service.config.DBConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringJUnitConfig(classes = {DBConfig.class})
 class RatesMapperTest {
     @Autowired
     private RatesMapper ratesMapper;
+
     @Test
-    void testFetchRates(){
+    void testFetchRates() {
         int a = 1;
         a++;
     }
